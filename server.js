7383ffb4f8568e3a8274f158643470bc19dff466
DrewStock <andrewdstock@gmail.com>
2016-11-14 13:42:36 -0800

    setup MongoDB(db name:fanclub) hosted by mLab

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const app = require('./lib/app');
 const http = require('http');
 const port = process.env.PORT || 3000;
-// const connection = require('./lib/setup-mongoose');
+require('./lib/setup-mongoose');
 
 const server = http.createServer(app);
 server.listen(port, () => {
