fd0e4e48e896c7e6a1aa9e1fc8ac9cfc675e13ae
willnickerson <wnickers@willamette.edu>
2016-11-15 14:24:58 -0800

    es6 re-added

--- server.js
+++ server.js
@@ -4,6 +4,6 @@ const port = process.env.PORT || 3000;
 require('./lib/setup-mongoose');
 
 const server = http.createServer(app);
-server.listen(port,  function() {
+server.listen(port, () => {
   console.log('server running at', server.address());
 });
