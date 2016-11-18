850926d7c45718478d9f1aeb369703121ab728ce
willnickerson <wnickers@willamette.edu>
2016-11-18 10:35:38 -0800

    add playlist to user spotify on client side is done

--- public/scripts/all-playlists.js
+++ public/scripts/all-playlists.js
@@ -1,14 +1,75 @@
 const renderAllPlaylists = function() {
-  $.ajax({ // eslint-disable-line
-    url: '/api/playlists', // Localhost removed
+  $.ajax({
+    url: '/api/playlists',
     success: successHandler,
     error: err => console.error(err)
   });
 
   function successHandler(data) {
-    console.log(data);
-    data.forEach(playlist => $('#all-playlists-list').append($('<li></li>').text(playlist.name).attr('data-id', playlist._id))); // eslint-disable-line
+    data.forEach(playlist => $('#all-playlists-list').append($('<li></li>').text(playlist.name).attr('data-id', playlist._id).addClass('selectable')));
+    selectAppPlaylist();
   }
 };
 
+const addPlaylistHandler = function() {
+  $('#add-playlist-button').on('click', event => {
+    event.preventDefault();
+    const $selected = $('#all-playlists-list .selected');
+    const playlistId = $selected[0].getAttribute('data-id');
+    const playlistName = $selected.text();
+
+    if($selected.length === 1) {
+      $.ajax({
+        url: `/api/sync-spotify/create-playlist/${playlistId}`,
+        success: successHandler,
+        error: err => console.log(err)
+      });
+    }
+    function successHandler() {
+      console.log(`You have added ${playlistName} to your Spotify`);
+      const currUserId = localStorage.getItem('currUserId');
+      console.log(currUserId);
+      $.ajax({
+        url: `/api/users/${currUserId}`,
+        success: successHandler,
+        error: err => console.log(err)
+      });
+
+      function successHandler(data) {
+        let playlists = [];
+        if(!data.playlists) {
+          playlists = [playlistId];
+        } else {
+          playlists = data.playlists.push(playlistId);
+        }
+        data.playlists = playlists
+        $.ajax({
+          type: 'PUT',
+          url: `/api/users/${currUserId}`,
+          data: JSON.stringify(data),
+          contentType: 'application/json',
+          success: successHandler,
+          error: err => console.log(err)
+        });
+
+        function successHandler(data) {
+          console.log(data);
+        }
+      }
+    }
+
+  });
+};
+
+const selectAppPlaylist = function() {
+  $('#all-playlists-list .selectable').on('click', event => {
+    event.preventDefault();
+    $('#all-playlists-list .selected').removeClass('selected');
+    var $target = $(event.target);
+    $target.addClass('selected');
+  });
+};
+
+
 renderAllPlaylists();
+addPlaylistHandler();
--- public/scripts/user-playlists.js
+++ public/scripts/user-playlists.js
@@ -1,26 +1,26 @@
 const handleGetPlaylists = function() {
-  $('#get-playlists-button').on('click', event => { // eslint-disable-line
+  $('#get-playlists-button').on('click', event => {
     event.preventDefault();
-    $.ajax({ // eslint-disable-line
-      url: '/api/sync-spotify/get-curr-user-playlists', // localhost Removed
+    $.ajax({
+      url: '/api/sync-spotify/get-curr-user-playlists',
       success: successHandler,
       error: err => console.log(err)
     });
 
     function successHandler(data) {
       data.forEach(playlist => {
-        $('#all-user-playlists').append($('<li></li>').text(playlist.name).attr('data-id', playlist.id).attr('data-user', playlist.owner.id).addClass('selectable')); // eslint-disable-line
+        $('#all-user-playlists').append($('<li></li>').text(playlist.name).attr('data-id', playlist.id).attr('data-user', playlist.owner.id).addClass('selectable'));
       });
-      selectPlaylist();
+      selectUserPlaylist();
     }
   });
 };
 
-const selectPlaylist = function() {
-  $('.selectable').on('click', event => { // eslint-disable-line
+const selectUserPlaylist = function() {
+  $('#all-user-playlists .selectable').on('click', event => {
     event.preventDefault();
-    $('.selected').removeClass('selected'); // eslint-disable-line
-    var $target = $(event.target); // eslint-disable-line
+    $('#all-user-playlists .selected').removeClass('selected');
+    var $target = $(event.target);
     $target.addClass('selected');
   });
 };
@@ -29,7 +29,7 @@ const handleUploadPlaylists = function() {
   $('#upload-playlist-button').on('click', event => { // eslint-disable-line
     const playlist = {};
     event.preventDefault();
-    if($('.selected').length === 1) { // eslint-disable-line
+    if($('#all-user-playlists .selected').length === 1) { // eslint-disable-line
       const $selected = $('.selected')[0]; // eslint-disable-line
       playlist.id = $selected.getAttribute('data-id');
       playlist.userId = $selected.getAttribute('data-user');
