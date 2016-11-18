b01bf57a075c3a98bc389466f5a8333bdc0aad81
DrewStock <andrewdstock@gmail.com>
2016-11-17 16:16:44 -0800

    updated PUT route for groups, updated groups UI to add a group and view all groups

--- public/scripts/publicGroups.js
+++ public/scripts/publicGroups.js
@@ -1,28 +1,80 @@
-const handleAddGroup = function() {
-  $('#addGroup-button').on('click', event => { // eslint-disable-line
-    console.log('addGroup clicked');
-    event.preventDefault();
-    const newGroupInfo = {
-      groupName: $('#groupName-input').val(), // eslint-disable-line
-      description: $('#groupDescription-input').val() // eslint-disable-line
-    };
-    $.ajax({ // eslint-disable-line
-      method: 'POST',
-      url: 'http://localhost:3000/api/groups',
-      data: JSON.stringify(newGroupInfo),
-      success: successHandler,
-      error: errorHandler,
-      contentType: 'application/json'
+(function(module) {
+
+  var publicGroups = {};
+
+  publicGroups.handleAddGroup = function() {
+    $('#addGroup-button').on('click', event => { // eslint-disable-line
+      console.log('addGroup clicked');
+      event.preventDefault();
+      const newGroupInfo = {
+        groupName: $('#groupName-input').val(), // eslint-disable-line
+        description: $('#groupDescription-input').val() // eslint-disable-line
+      };
+      $.ajax({ // eslint-disable-line
+        method: 'POST',
+        url: 'http://localhost:3000/api/groups',
+        data: JSON.stringify(newGroupInfo),
+        success: successHandler,
+        error: errorHandler,
+        contentType: 'application/json'
+      });
+
+      function successHandler(data) {
+        console.log(data);
+      }
+
+      function errorHandler(err) {
+        console.log('ERROR', err);
+      }
     });
+  };
 
-    function successHandler(data) {
-      console.log(data);
-    }
+  publicGroups.allGroups = [];
 
-    function errorHandler(err) {
-      console.log('ERROR', err);
-    }
-  });
-};
+  publicGroups.displayAll = function() {
+    $('#displayGroups-button').on('click', event => { // eslint-disable-line
+      console.log('displayGroups clicked');
+      event.preventDefault();
+      $.ajax({ // eslint-disable-line
+        method: 'GET',
+        url: 'http://localhost:3000/api/groups',
+        data: JSON.stringify,
+        success: successHandler,
+        error: errorHandler,
+        contentType: 'application/json'
+      });
 
-handleAddGroup();
+      function successHandler(data) {
+        publicGroups.allGroups = [data];
+        console.log(data);
+        publicGroups.appendAll();
+      }
+
+      function errorHandler(err) {
+        console.log('ERROR', err);
+      }
+    });
+  };
+
+  publicGroups.render = function(getGroups) {
+    var template = Handlebars.compile($('#displayGroups-template').text()); // eslint-disable-line
+    return template(getGroups);
+  };
+
+  publicGroups.appendAll = function() {
+    // publicGroups.searchResultsQuery();
+    publicGroups.allGroups[0].forEach(function(group) {
+      $('#all-groups-container ul').append(publicGroups.render(group)); // eslint-disable-line
+    });
+  };
+
+
+
+  publicGroups.handleAddGroup();
+
+  publicGroups.displayAll();
+
+
+  module.publicGroups = publicGroups;
+
+})(window); // eslint-disable-line
